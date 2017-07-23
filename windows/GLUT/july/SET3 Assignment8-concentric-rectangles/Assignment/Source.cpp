27375667308d44f15eb2aeaf92a3fb19bf115825
hriships <hrishishinde.p@gmail.com>
2017-07-23 06:38:03 -0700

    added concentric rectangles

---
+++ windows/GLUT/july/SET3 Assignment8-concentric-rectangles/Assignment/Source.cpp
@@ -0,0 +1,159 @@
+#include <GL/freeglut.h>
+
+#define WIN_WIDTH 600
+#define WIN_HEIGHT 600
+
+bool gFulllScreen = false;
+
+int main(int argc, char** argv)
+{
+	int iScreenWidth, iScreenHeight;
+
+	// function prototypes
+	void display(void);
+	void resize(int, int);
+	void keyboard(unsigned char, int, int);
+	void mouse(int, int, int, int);
+	void initialize(void);
+	void uninitilize(void);
+
+	//code
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+	iScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+	iScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+	glutInitWindowPosition((iScreenWidth/2) - (WIN_WIDTH/2),
+							(iScreenHeight/2) - (WIN_HEIGHT/2));
+	glutCreateWindow("OpenGL GLUT: Concentric Rectangles");
+
+	initialize();
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
+	glutCloseFunc(uninitilize);
+
+	glutMainLoop();
+
+	// return (0);
+}
+
+
+void initialize(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void display(void)
+{
+	void renderConcentricRectangles(void);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	renderConcentricRectangles();
+
+	glFlush();
+}
+
+void renderConcentricRectangles(void)
+{
+	void drawRectangle(GLfloat, GLfloat[]);
+	int i;
+	GLfloat tringleColors[10][3] = {
+		{ 1.0f, 0.0f, 0.0f },
+		{ 0.0f, 1.0f, 0.0f },
+		{ 0.0f, 0.0f, 1.0f },
+		{ 1.0f, 1.0f, 0.0f },
+		{ 0.0f, 1.0f, 1.0f },
+		{ 1.0f, 0.0f, 1.0f },
+		{ 0.2f, 0.8f, 0.8f },
+		{ 0.5f, 0.0f, 0.5f },
+		{ 1.0f, 0.5f, 0.2f },
+		{ 1.0f, 0.1f, 0.2f }
+	};
+
+	for (i = 1; i <= 10; i++)
+	{
+		drawRectangle(i*0.1f, tringleColors[(i - 1)]);
+	}
+}
+
+void drawRectangle(GLfloat vertexPos, GLfloat colorValue[])
+{
+	glLineWidth(2);
+	glBegin(GL_LINES);
+		glColor3f(colorValue[0], colorValue[1], colorValue[2]);
+
+		glVertex3f(-vertexPos, vertexPos, 0.0f);
+		glVertex3f(-vertexPos, -vertexPos, 0.0f);
+
+		glVertex3f(-vertexPos, -vertexPos, 0.0f);
+		glVertex3f(vertexPos, -vertexPos, 0.0f);
+
+		glVertex3f(vertexPos, -vertexPos, 0.0f);
+		glVertex3f(vertexPos, vertexPos, 0.0f);
+
+		glVertex3f(vertexPos, vertexPos, 0.0f);
+		glVertex3f(-vertexPos, vertexPos, 0.0f);
+	glEnd();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:// escape
+	case 'Q':
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+
+	case 'F':
+	case 'f':
+		if (gFulllScreen == false)
+		{
+			glutFullScreen();
+		}
+		else 
+		{
+			glutLeaveFullScreen();
+		}
+		gFulllScreen = !(gFulllScreen);
+		break;
+
+	default:
+		break;
+	}
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	switch (button)
+	{
+	case GLUT_LEFT_BUTTON:
+		break;
+	default:
+		break;
+	}
+}
+
+
+void resize(int width, int height)
+{
+	if (height == 0)
+		height = 1;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+}
+
+
+void uninitilize()
+{
+
+}
