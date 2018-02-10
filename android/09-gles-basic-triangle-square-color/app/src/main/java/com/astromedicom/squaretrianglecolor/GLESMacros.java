259a1ca02a453d404a1c034bda54f1f78c975817
hriships <hrishishinde.p@gmail.com>
2018-02-10 16:41:08 +0530

    added colored triangle and square

---
+++ android/09-gles-basic-triangle-square-color/app/src/main/java/com/astromedicom/squaretrianglecolor/GLESMacros.java
@@ -0,0 +1,10 @@
+package com.astromedicom.squaretrianglecolor;
+
+/**
+ * Created by Hrishikesh on 06-02-2018.
+ */
+
+class GLESMacros {
+    public static final int VDG_ATTRIBUTE_VERTEX = 0;
+    public static final int VDG_ATTRIBUTE_COLOR = 1;
+}
