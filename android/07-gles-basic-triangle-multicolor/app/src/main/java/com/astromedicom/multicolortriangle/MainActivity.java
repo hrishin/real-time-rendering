d691abe545ba5475f24768ef744a82825df5f412
hriships <hrishishinde.p@gmail.com>
2018-02-10 16:07:06 +0530

    added multicolor triangele

---
+++ android/07-gles-basic-triangle-multicolor/app/src/main/java/com/astromedicom/multicolortriangle/MainActivity.java
@@ -0,0 +1,38 @@
+package com.astromedicom.multicolortriangle;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    private GLESView glesView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        removeTitleBar();
+        makeFullScreen();
+        makeLandscape();
+        glesView = new GLESView(this);
+        setContentView(glesView);
+    }
+
+    private void removeTitleBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    private void makeFullScreen() {
+        getWindow()
+                .setFlags(
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN
+                );
+    }
+
+    private void makeLandscape() {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
