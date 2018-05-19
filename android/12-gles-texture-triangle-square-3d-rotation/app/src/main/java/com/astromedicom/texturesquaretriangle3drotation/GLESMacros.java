9bae0828d3cc8e5ce17ea41a7e5baffc10f063cb
hriships <hrishishinde.p@gmail.com>
2018-05-19 17:44:48 +0530

    added android texture 3D assignment

---
+++ android/12-gles-texture-triangle-square-3d-rotation/app/src/main/java/com/astromedicom/texturesquaretriangle3drotation/GLESMacros.java
@@ -0,0 +1,11 @@
+package com.astromedicom.texturesquaretriangle3drotation;
+
+/**
+ * Created by Hrishikesh on 06-02-2018.
+ */
+
+class GLESMacros {
+    public static final int VDG_ATTRIBUTE_VERTEX = 0;
+    public static final int VDG_ATTRIBUTE_COLOR = 1;
+    public static final int VDG_ATTRIBUTE_TEXTURE0 = 2;
+}
---
+++ android/12-gles-texture-triangle-square-3d-rotation/app/src/main/java/com/astromedicom/texturesquaretriangle3drotation/TextureHandler.java
@@ -0,0 +1,8 @@
+package com.astromedicom.texturesquaretriangle3drotation;
+
+/**
+ * Created by Hrishikesh on 19-05-2018.
+ */
+
+public class TextureHandler {
+}
