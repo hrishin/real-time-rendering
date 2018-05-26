83886ca2bb7fae213a2191e8a4cf77b9aac06940
hrishin <hrishihsinde.p@gmail.com>
2018-05-26 18:44:53 +0530

    updated gitignore

---
+++ macOS/FirstWindow/FirstWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstWindow
+//
+//  Created by Hrishikesh Shinde on 26/05/18.
+//  Copyright © 2018 Hrishikesh Shinde. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
