06149a2a60654bdf873d94fba3289fd8de0548ab
hrishin <hrishishinde.p@gmail.com>
2018-07-07 18:39:02 +0530

    multi vao and color example

---
+++ macOS/7.multicolor-square-triangle/glview.h
@@ -0,0 +1,25 @@
+#import <Cocoa/Cocoa.h>
+
+#import <QuartzCore/CVDisplayLink.h>
+
+#import <OpenGL/gl3.h>
+#import <OpenGL/gl3ext.h>
+
+#import "vmath.h"
+
+// 'C' style global declaration
+CVReturn MyDisplayLinkCallback(CVDisplayLinkRef, const CVTimeStamp *, const CVTimeStamp *,
+                      CVOptionFlags, CVOptionFlags *, void *);
+extern FILE *gpFile;
+
+enum 
+{
+	VDG_ATTRIBUTE_VERTEX = 0,
+	VDG_ATTRIBUTE_COLOR,
+	VDG_ATTRIBUTE_NORMAL,
+	VDG_ATTRIBUTE_TETURES
+};
+
+@interface GLView : NSOpenGLView
+
+@end
