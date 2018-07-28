308dffe432053b8839d41632f2e4403da62ca785
hriships <hrishishinde.p@gmail.com>
2018-07-28 15:26:42 +0530

    android: pyramid 2 lights

---
+++ android/20-gles-2-light-pyramid/app/src/main/java/com/astromedicom/lightpervertex/GLESMacros.java
@@ -0,0 +1,12 @@
+package com.astromedicom.lightpervertex;
+
+/**
+ * Created by Hrishikesh on 06-02-2018.
+ */
+
+class GLESMacros {
+    public static final int VDG_ATTRIBUTE_VERTEX = 0;
+    public static final int VDG_ATTRIBUTE_COLOR = 1;
+    public static final int VDG_ATTRIBUTE_TEXTURE0 = 2;
+    public static final int VDG_ATTRIBUTE_NORMAL = 3;
+}
