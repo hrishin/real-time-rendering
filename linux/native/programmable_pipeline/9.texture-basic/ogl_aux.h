fabadec1b787707164c844e5a216066e83162467
hrishin <hrishishinde.p@gmail.com>
2018-08-18 22:24:38 +0530

    deleted duplicated assignment

--- linux/native/programmable_pipeline/9.texture-basic/ogl_aux.h
+++
@@ -1,9 +0,0 @@
-#ifndef OGL_AUX_H
-#define OGL_AUX_H
-
-extern void Uninitialize(void);
-
-void LogShaderCompilationStatus(GLuint, FILE *, const char *);
-void LogProgramCompilationStatus(GLuint, FILE *, const char *);
-
-#endif
