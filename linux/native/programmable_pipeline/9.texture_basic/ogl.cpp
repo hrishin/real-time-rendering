637e14ca472addfd9495c235b3804923d52e71f0
hrishin <hrishishinde.p@gmail.com>
2018-08-18 18:42:07 +0530

    linux - basic texture, smile please :)

---
+++ linux/native/programmable_pipeline/9.texture_basic/ogl.cpp
@@ -0,0 +1,599 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/XKBlib.h>
+#include <X11/keysym.h>
+
+#include <GL/glew.h>
+#include <GL/glx.h>
+#include <SOIL/SOIL.h>
+
+#include "vmath.h"
+
+#include "ogl_aux.h"
+
+using namespace std;
+using namespace vmath;
+
+enum
+{
+    VDG_ATTRIBUTE_VERTEX = 0,
+    VDG_ATTRIBUTE_COLOR,
+    VDG_ATTRIBUTE_NORMAL,
+    VDG_ATTRIBUTE_TEXTURE0,
+};
+
+// windowing properties
+Display *gpDisplay = NULL;
+XVisualInfo *gpXVisualInfo = NULL;
+Colormap gColorMap;
+Window gWindow;
+int giWindowWidth = 800;
+int giWindowHeight = 600;
+const char *gpWindowTitle = "Programmable Pipeline : Texture Smiley";
+
+GLXContext gGLXContext;
+
+GLuint gVertexShaderObject;
+GLuint gFragmentShaderObject;
+GLuint gShaderProgramObject;
+
+GLuint gVaoSquare;
+GLuint gVboSquarePosition;
+GLuint gVboSquareTexture;
+
+GLuint gMVPUniform;
+GLuint gTextureSamplerUniform;
+GLuint gTextureSmily;
+
+mat4 gPerspectiveProjectionMatrix;
+
+FILE *gpLogFile;
+bool gbFullScreen = false;
+
+// entry point function
+int main(int argc, char *arg[])
+{
+    // function prototypes
+    void CreateWindow(void);
+    void ToggleFullscreen(void);
+    void Initialize(void);
+    void Render(void);
+    void Resize(int, int);
+    void Uninitialize(void);
+
+    // varible declearation
+    int winWidth = giWindowWidth;
+    int winHeight = giWindowHeight;
+
+    bool bDone = false;
+
+    CreateWindow();
+
+    Initialize();
+
+    // Game loop
+    XEvent event;
+    KeySym keysym;
+
+    while (bDone == false)
+    {
+        while (XPending(gpDisplay))
+        {
+            XNextEvent(gpDisplay, &event);
+            switch (event.type)
+            {
+            case MapNotify:
+                break;
+
+            case KeyPress:
+                keysym = XkbKeycodeToKeysym(gpDisplay, event.xkey.keycode, 0, 0);
+                switch (keysym)
+                {
+                case XK_Escape:
+                    bDone = true;
+                    break;
+
+                case XK_F:
+                case XK_f:
+                    if (gbFullScreen == false)
+                    {
+                        ToggleFullscreen();
+                        gbFullScreen = true;
+                    }
+                    else
+                    {
+                        ToggleFullscreen();
+                        gbFullScreen = false;
+                    }
+                    break;
+
+                default:
+                    break;
+                }
+                break;
+
+            case ButtonPress:
+                switch (event.xbutton.button)
+                {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    break;
+                }
+                break;
+
+            case MotionNotify:
+                break;
+
+            case ConfigureNotify:
+                winWidth = event.xconfigure.width;
+                winHeight = event.xconfigure.height;
+                Resize(winWidth, winHeight);
+                break;
+
+            case Expose:
+                break;
+
+            case DestroyNotify:
+                break;
+
+            case 33:
+                bDone = true;
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        Render();
+    }
+
+    Uninitialize();
+    return (EXIT_SUCCESS);
+}
+
+void CreateWindow(void)
+{
+    void Uninitialize(void);
+
+    //varible declarations
+    XSetWindowAttributes winAttribs;
+    int defaultScreen;
+    int styleMask;
+
+    static int frameBufferAttributes[] =
+        {
+            GLX_RGBA,
+            GLX_RED_SIZE, 1,
+            GLX_GREEN_SIZE, 1,
+            GLX_BLUE_SIZE, 1,
+            GLX_ALPHA_SIZE, 1,
+            GLX_DEPTH_SIZE, 24,
+            GLX_DOUBLEBUFFER, True,
+            None};
+
+    gpDisplay = XOpenDisplay(NULL);
+    if (gpDisplay == NULL)
+    {
+        fprintf(stderr, "ERROR : Unable to Open X Display. \nExiting now...\n");
+        Uninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    defaultScreen = XDefaultScreen(gpDisplay);
+
+    gpXVisualInfo = glXChooseVisual(gpDisplay, defaultScreen, frameBufferAttributes);
+    if (gpXVisualInfo == NULL)
+    {
+        fprintf(stderr, "ERROR: Unable to get a Visual Info.\nExiting now...\n");
+        Uninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    winAttribs.border_pixel = 0;
+    winAttribs.background_pixmap = 0;
+    winAttribs.colormap = XCreateColormap(gpDisplay, RootWindow(gpDisplay, gpXVisualInfo->screen), gpXVisualInfo->visual, AllocNone);
+    gColorMap = winAttribs.colormap;
+    winAttribs.background_pixel = BlackPixel(gpDisplay, defaultScreen);
+    winAttribs.event_mask = ExposureMask | VisibilityChangeMask | ButtonPressMask | KeyPressMask | PointerMotionMask | StructureNotifyMask;
+
+    styleMask = CWBorderPixel | CWBackPixel | CWEventMask | CWColormap;
+
+    gWindow = XCreateWindow(gpDisplay,
+                            RootWindow(gpDisplay, gpXVisualInfo->screen),
+                            0,
+                            0,
+                            giWindowWidth,
+                            giWindowHeight,
+                            0,
+                            gpXVisualInfo->depth,
+                            InputOutput,
+                            gpXVisualInfo->visual,
+                            styleMask,
+                            &winAttribs);
+
+    if (!gWindow)
+    {
+        printf("ERROR : Failed to creat main window.\nExiting now...\n");
+        Uninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    XStoreName(gpDisplay, gWindow, gpWindowTitle);
+
+    Atom windowManagerDelete = XInternAtom(gpDisplay, "WM_DELETE_WINDOW", True);
+    XSetWMProtocols(gpDisplay, gWindow, &windowManagerDelete, 1);
+
+    XMapWindow(gpDisplay, gWindow);
+}
+
+void Initialize(void)
+{
+    void Resize(int, int);
+    bool LoadTexture(GLuint *, const char *);
+    void Uninitialize(void);
+    void logShaderCompilationStatus(GLuint, FILE *, const char *);
+    void logProgramCompilationStatus(GLuint, FILE *, const char *);
+
+    gpLogFile = fopen("log.txt", "w");
+    if (gpLogFile)
+    {
+        fprintf(gpLogFile, "Log file created successfully \n");
+    }
+    else
+    {
+        fprintf(stderr, "Log file can't be created \n exiting \n");
+        exit(EXIT_FAILURE);
+    }
+
+    gGLXContext = glXCreateContext(gpDisplay, gpXVisualInfo, NULL, GL_TRUE);
+    if (gGLXContext == NULL)
+    {
+        fprintf(stderr, "ERROR: Unable to create OpenGL context.\nExiting now...\n");
+        Uninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    if (glXMakeCurrent(gpDisplay, gWindow, gGLXContext) == False)
+    {
+        fprintf(stderr, "ERROR: Unable to set the current context GL context.\nExiting now...\n");
+        Uninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    // GLEW initialization
+    GLenum glewError = glewInit();
+    if (glewError != GLEW_OK)
+    {
+        fprintf(stderr, "ERROR: Unable to initialize extension wranglers (GLEW).\nExiting now...\n");
+        Uninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    // VERTEX SHADER
+    gVertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar *vertexShaderSourceCode =
+        "#version 130"
+        "\n"
+        "in vec4 vPosition;"
+        "in vec2 vTexture0_coords;"
+        "out vec2 out_texture0_coords;"
+        "uniform mat4 u_mvp_matrix;"
+        "void main(void)"
+        "{"
+        "gl_Position = u_mvp_matrix * vPosition;"
+        "out_texture0_coords = vTexture0_coords;"
+        "}";
+    glShaderSource(gVertexShaderObject, 1, (const GLchar **)&vertexShaderSourceCode, NULL);
+    glCompileShader(gVertexShaderObject);
+    LogShaderCompilationStatus(gVertexShaderObject, gpLogFile, "Vertex Shader Compilation Log");
+
+    // FRAGMENT SHADER
+    gFragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar *fragmentShaderSourceCode =
+        "#version 130"
+        "\n"
+        "in vec2 out_texture0_coords;"
+        "out vec4 FragColor;"
+        "uniform sampler2D u_texture0_sampler;"
+        "void main(void)"
+        "{"
+        "FragColor = texture(u_texture0_sampler, out_texture0_coords);"
+        "}";
+    glShaderSource(gFragmentShaderObject, 1, (const GLchar **)&fragmentShaderSourceCode, NULL);
+    glCompileShader(gFragmentShaderObject);
+    LogShaderCompilationStatus(gFragmentShaderObject, gpLogFile, "Fragment Shader Compilation Log");
+
+    // SHADER PROGRAM
+    gShaderProgramObject = glCreateProgram();
+
+    // attach vertex shader to shader program
+    glAttachShader(gShaderProgramObject, gVertexShaderObject);
+
+    // attach vertex shader to shader program
+    glAttachShader(gShaderProgramObject, gFragmentShaderObject);
+
+    // pre-link binding of shader program object with vertex shader "position" attribute
+    glBindAttribLocation(gShaderProgramObject, VDG_ATTRIBUTE_VERTEX, "vPosition");
+
+    // pre-link binding of shader program object with vertex shader "texture" attribute
+    glBindAttribLocation(gShaderProgramObject, VDG_ATTRIBUTE_TEXTURE0, "vTexture0_coords");
+
+    // link shader
+    glLinkProgram(gShaderProgramObject);
+    LogProgramCompilationStatus(gShaderProgramObject, gpLogFile, "Shader Program Link Log");
+
+    // get MVP uniform location
+    gMVPUniform = glGetUniformLocation(gShaderProgramObject, "u_mvp_matrix");
+    gTextureSamplerUniform = glGetUniformLocation(gShaderProgramObject, "u_texture0_sampler");
+
+    // vertices, colors, shader attribs, vbo, vao initializations
+	const GLfloat squareVertices[] =
+	{
+		1.0f, 1.0f, 1.0f, // front
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+	};
+
+	// pretty bullie way
+	const GLfloat squareTextureCords[] =
+	{
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+	};	
+
+	// Cube VAO
+	glGenVertexArrays(1, &gVaoSquare);
+	glBindVertexArray(gVaoSquare);
+
+	// Position
+	glGenBuffers(1, &gVboSquarePosition);
+	glBindBuffer(GL_ARRAY_BUFFER, gVboSquarePosition);
+	// move data from main memory to graphics memory
+	// GL_STATIC_DRAW or GL_DYNAMIC_DRAW : How you want load data run or preloading. example game shows loadingbar and "loading" messages
+	glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(VDG_ATTRIBUTE_VERTEX, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(VDG_ATTRIBUTE_VERTEX);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Texture
+	glGenBuffers(1, &gVboSquareTexture);
+	glBindBuffer(GL_ARRAY_BUFFER, gVboSquareTexture);
+	// move data from main memory to graphics memory
+	// GL_STATIC_DRAW or GL_DYNAMIC_DRAW : How you want load data run or preloading. example game shows loadingbar and "loading" messages
+	glBufferData(GL_ARRAY_BUFFER, sizeof(squareTextureCords), squareTextureCords, GL_STATIC_DRAW);
+	glVertexAttribPointer(VDG_ATTRIBUTE_TEXTURE0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(VDG_ATTRIBUTE_TEXTURE0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    // perspective matrix to identity matrix
+    gPerspectiveProjectionMatrix = mat4::identity();
+
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    //glEnable(GL_CULL_FACE);
+
+    LoadTexture(&gTextureSmily, "Smiley-512x512.bmp");
+
+    // warm up
+    Resize(giWindowWidth, giWindowHeight);
+}
+
+bool LoadTexture(GLuint *texture, const char *image)
+{
+    bool is_status = false;
+    int width, height;
+    unsigned char *imgData = NULL;
+
+    glGenTextures(1, texture);
+    imgData = SOIL_load_image(image, &width, &height, 0, SOIL_LOAD_RGB);
+    if (imgData != NULL)
+    {
+        is_status = true;
+        glBindTexture(GL_TEXTURE_2D, *texture);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, (void *)imgData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        SOIL_free_image_data(imgData);
+    }
+
+    return (is_status);
+}
+
+void ToggleFullscreen(void)
+{
+    Atom wm_state;
+    Atom fullscreen;
+    XEvent xev = {0};
+
+    wm_state = XInternAtom(gpDisplay, "_NET_WM_STATE", False);
+    memset(&xev, 0, sizeof(xev));
+
+    xev.type = ClientMessage;
+    xev.xclient.window = gWindow;
+    xev.xclient.message_type = wm_state;
+    xev.xclient.format = 32;
+    xev.xclient.data.l[0] = gbFullScreen ? 0 : 1;
+
+    fullscreen = XInternAtom(gpDisplay, "_NET_WM_STATE_FULLSCREEN", False);
+    xev.xclient.data.l[1] = fullscreen;
+
+    XSendEvent(gpDisplay,
+               RootWindow(gpDisplay, gpXVisualInfo->screen),
+               False,
+               StructureNotifyMask,
+               &xev);
+}
+
+void Render(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(gShaderProgramObject);
+
+	// OpenGL Drawing for Traingle
+	// set modelview & modelviewprojection matrices to identity
+	mat4 modelViewMatrix = mat4::identity();
+	mat4 modelViewProjectionMatrix = mat4::identity();
+
+	// tranlsate to negative x, z axis
+	modelViewMatrix = modelViewMatrix * vmath::translate(0.0f, 0.0f, -5.0f);
+
+	// multiply the modelview and orthographic matrix to get modelviewprojection matri
+	// order is important
+	modelViewProjectionMatrix = gPerspectiveProjectionMatrix * modelViewMatrix;
+
+	// pass above modelviewprojection matrix to the vertex shader in 'u_mvp_matrix' shader variable
+	// whose position value we already calculated in initWithFrame() by using glGetUniformLocation()
+	glUniformMatrix4fv(gMVPUniform, 1, GL_FALSE, modelViewProjectionMatrix);
+
+	// bind texture
+	glActiveTexture(GL_TEXTURE0); // OpenGL spec says's 80 textures units, it could work for with specifying for single texture
+	glBindTexture(GL_TEXTURE_2D, gTextureSmily); // which texture to use
+	glUniform1i(gTextureSamplerUniform, 0); // 1i -> integer because sample2d is internally is integer, second parameter is texture index
+
+	// bind vao
+	glBindVertexArray(gVaoSquare);
+
+	// draw, either by glDrawTriangles() or glDrawArrays() or glDrawElements()
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4); // 3 (each with its x,y,z ) vertices in pyramidVertices array for 4 sides
+									     
+	glBindVertexArray(0); // unbind VAO
+
+    glUseProgram(0);
+
+    glXSwapBuffers(gpDisplay, gWindow);
+}
+
+void Resize(int width, int height)
+{
+    if (height == 0)
+    {
+        height = 1;
+    }
+
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+    gPerspectiveProjectionMatrix = vmath::perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+
+void Uninitialize(void)
+{
+    GLXContext currentGLXContext;
+
+    // destroy vao
+	if (gVaoSquare)
+	{
+		glDeleteVertexArrays(1, &gVaoSquare);
+		gVaoSquare = 0;
+	}
+
+	// destroy vbo
+	if (gVboSquarePosition)
+	{
+		glDeleteBuffers(1, &gVboSquarePosition);
+		gVboSquarePosition = 0;
+	}
+	
+	if (gVboSquareTexture)
+	{
+		glDeleteBuffers(1, &gVboSquareTexture);
+		gVboSquareTexture = 0;
+	}
+
+    if (gShaderProgramObject && gVertexShaderObject)
+    {
+        glDetachShader(gShaderProgramObject, gVertexShaderObject);
+    }
+
+    if (gShaderProgramObject && gFragmentShaderObject)
+    {
+        glDetachShader(gShaderProgramObject, gFragmentShaderObject);
+    }
+
+    if (gVertexShaderObject)
+    {
+        glDeleteShader(gVertexShaderObject);
+        gVertexShaderObject = 0;
+    }
+
+    if (gFragmentShaderObject)
+    {
+        glDeleteShader(gFragmentShaderObject);
+        gFragmentShaderObject = 0;
+    }
+
+    if (gShaderProgramObject)
+    {
+        glDeleteProgram(gShaderProgramObject);
+        gShaderProgramObject = 0;
+        glUseProgram(0);
+    }
+
+    currentGLXContext = glXGetCurrentContext();
+
+    if (currentGLXContext != NULL && currentGLXContext != gGLXContext)
+    {
+        glXMakeCurrent(gpDisplay, 0, 0);
+    }
+
+    if (gGLXContext)
+    {
+        glXDestroyContext(gpDisplay, gGLXContext);
+    }
+
+    if (gWindow)
+    {
+        XDestroyWindow(gpDisplay, gWindow);
+    }
+
+    if (gColorMap)
+    {
+        XFreeColormap(gpDisplay, gColorMap);
+    }
+
+    if (gpXVisualInfo)
+    {
+        free(gpXVisualInfo);
+        gpXVisualInfo = NULL;
+    }
+
+    if (gpDisplay)
+    {
+        XCloseDisplay(gpDisplay);
+        gpDisplay = NULL;
+    }
+
+    if (gpLogFile)
+    {
+        fprintf(gpLogFile, "Log file is successfully closed\n");
+        fclose(gpLogFile);
+        gpLogFile = NULL;
+    }
+}
