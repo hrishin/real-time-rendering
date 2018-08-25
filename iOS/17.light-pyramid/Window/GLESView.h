b3a2f8428a81229aef395c1e10fa421b5be8da40
hrishin <hrishishinde.p@gmail.com>
2018-08-25 15:32:26 +0530

    iOS - pyramid lighting

---
+++ iOS/17.light-pyramid/Window/GLESView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  Window
+//
+//  Created by Hrishikesh Shinde on 22/08/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLESView : UIView <UIGestureRecognizerDelegate>
+    - (void) startAnimation;
+    - (void) stopAnimation;
+@end
